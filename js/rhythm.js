8e8439645b950009f6597d1379984577d3ae0231
Vershima kwaghshi <kwaghshivershimasolomon@gmail.com>
2026-07-04 07:08:05 +0100

    Update rhythm.js

--- js/rhythm.js
+++ js/rhythm.js
@@ -1,1 +1,15 @@
+export class RhythmEngine {
+    constructor() {
+        this.beat = 0;
+        this.tempo = 60; // BPM
+    }
+
+    update(delta) {
+        this.beat += delta * (this.tempo / 60);
+    }
+    
+    setTempo(bpm) {
+        this.tempo = bpm;
+    }
+}
 
