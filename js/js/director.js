6d54650d0428b96c27d62f6a3a936e661377c58d
Vershima kwaghshi <kwaghshivershimasolomon@gmail.com>
2026-07-04 07:08:38 +0100

    Update director.js

--- js/js/director.js
+++ js/js/director.js
@@ -1,1 +1,18 @@
+export class Director {
+    constructor(world, rhythm) {
+        this.world = world;
+        this.rhythm = rhythm;
+        this.queue = [];
+        this.currentMoment = null;
+    }
+
+    cue(moment) {
+        this.currentMoment = moment;
+        // Logic for triggering World/Camera/Type states
+    }
+
+    update(delta) {
+        // Orchestration logic
+    }
+}
 
