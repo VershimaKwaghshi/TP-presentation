b0e9a83a784dd811f556854a0c9b0255563c0db7
Vershima kwaghshi <kwaghshivershimasolomon@gmail.com>
2026-07-04 07:09:14 +0100

    Update world.js

--- js/world.js
+++ js/world.js
@@ -1,1 +1,25 @@
+import * as THREE from 'three';
+import { Atmosphere } from './atmosphere.js';
+
+export class World {
+    constructor() {
+        this.scene = new THREE.Scene();
+        this.scene.fog = new THREE.FogExp2(0x000000, 0.08);
+        
+        this.renderer = new THREE.WebGLRenderer({ antialias: true, canvas: document.getElementById('world-canvas') });
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.renderer.toneMapping = THREE.ACESFilmicToneMapping;
+        
+        this.atmosphere = new Atmosphere(this.scene);
+    }
+
+    update(delta) {
+        this.atmosphere.update(delta);
+    }
+
+    render(camera) {
+        this.renderer.render(this.scene, camera);
+    }
+}
 
