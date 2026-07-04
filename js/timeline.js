32d722fd211ccd3ea702ddc5e1003a5dac60afb0
Vershima kwaghshi <kwaghshivershimasolomon@gmail.com>
2026-07-04 12:46:15 +0100

    Update timeline.js

--- js/timeline.js
+++ js/timeline.js
@@ -1,196 +1,80 @@
-export const Timeline = [export const Timeline = [
-
-{
-id:0,
-text:"Every day.",
-actor:"whisper",
-camera:"hero",
-world:"pressure"
-},
-
-{
-id:1,
-text:"Someone discovers trading.",
-actor:"reveal",
-camera:"push",
-world:"pressure"
-},
-
-{
-id:2,
-text:"People dream.",
-actor:"whisper",
-camera:"left",
-world:"truth"
-},
-
-{
-id:3,
-text:"Millions enter the market with hope.",
-actor:"reveal",
-camera:"right",
-world:"truth"
-},
-
-{
-id:4,
-text:"Most never return.",
-actor:"collapse",
-camera:"push",
-world:"pressure"
-},
-
-{
-id:5,
-text:"Not because they lack intelligence.",
-actor:"whisper",
-camera:"hero",
-world:"pressure"
-},
-
-{
-id:6,
-text:"Because no human was built to remain rational under financial pressure.",
-actor:"collapse",
-camera:"push",
-world:"pressure"
-},
-
-{
-id:7,
-text:"What if emotion never touched capital?",
-actor:"assemble",
-camera:"orbitLeft",
-world:"hope"
-},
-
-{
-id:8,
-text:"TAKE PROFIT",
-actor:"hero",
-camera:"push",
-world:"hope"
-},
-
-{
-chapter:2
-}
-
-];
+export const Timeline = [
 
     {
         id: 0,
-
         text: "Every day.",
-
         actor: "whisper",
-
         camera: "hero",
-
         duration: 2500
     },
 
     {
         id: 1,
-
         text: "Someone discovers trading.",
-
         actor: "reveal",
-
         camera: "push",
-
         duration: 3000
     },
 
     {
         id: 2,
-
         text: "People dream.",
-
         actor: "whisper",
-
         camera: "left",
-
         duration: 2500
     },
 
     {
         id: 3,
-
         text: "Millions enter the market with hope.",
-
         actor: "reveal",
-
         camera: "right",
-
         duration: 3500
     },
 
     {
         id: 4,
-
         text: "Most never return.",
-
         actor: "collapse",
-
         camera: "push",
-
         duration: 3500
     },
 
     {
         id: 5,
-
         text: "Not because they lack intelligence.",
-
         actor: "whisper",
-
         camera: "hero",
-
         duration: 4000
     },
 
     {
         id: 6,
-
         text: "Because no human was built to remain rational under financial pressure.",
-
         actor: "collapse",
-
         camera: "push",
-
         duration: 6000
     },
 
     {
         id: 7,
-
         text: "What if emotion never touched capital?",
-
         actor: "assemble",
-
         camera: "orbitLeft",
-
         duration: 5000
     },
 
     {
         id: 8,
-
         text: "TAKE PROFIT",
-
         actor: "hero",
-
         camera: "push",
-
         duration: 7000
     },
 
     {
         id: 9,
-
         chapter: 2
-
     }
 
 ];
