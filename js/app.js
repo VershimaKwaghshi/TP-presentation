1c21baea12d47a504ee5a2be7ba9b8b9d6eff1ca
Vershima kwaghshi <kwaghshivershimasolomon@gmail.com>
2026-07-04 08:32:13 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,30 +4,65 @@ import { Director } from './director.js';
 import { RhythmEngine } from './rhythm.js';
 
 class App {
+
     constructor() {
-        this.world = new World();
-        this.rhythm = new RhythmEngine();
-        this.director = new Director(this.world, this.rhythm);
+
         this.clock = new THREE.Clock();
 
-        this.init();
+        this.world = new World();
+
+        this.rhythm = new RhythmEngine();
+
+        this.director = new Director(
+            this.world,
+            this.rhythm
+        );
+
+        this.bindRhythm();
+
+        this.animate();
+
     }
 
-    init() {
-        this.animate();
+    bindRhythm() {
+
+        this.rhythm.subscribe((state) => {
+
+            document.body.style.backgroundPosition =
+                `${Math.sin(state.time * 0.2) * 30}px ${Math.cos(state.time * 0.15) * 30}px`;
+
+            const glow =
+                0.12 + state.pulse * 0.08;
+
+            document.documentElement.style.setProperty(
+                "--pulse",
+                glow
+            );
+
+        });
+
     }
 
     animate() {
+
         requestAnimationFrame(() => this.animate());
+
         const delta = this.clock.getDelta();
-        
+
         this.rhythm.update(delta);
-        this.world.update(delta);
+
         this.director.update(delta);
-        
+
+        this.world.update(delta);
+
         this.world.render();
+
     }
+
 }
 
-new App();
+window.addEventListener("load", () => {
 
+    new App();
+
+});
